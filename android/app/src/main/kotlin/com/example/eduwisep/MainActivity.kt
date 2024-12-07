3a0953724265cf9b2c00f6ae04527506b92670e9
AJ-1001 <asimjasim290901@gmail.com>
2024-12-08 02:46:25 +0530

    reborn

---
+++ android/app/src/main/kotlin/com/example/eduwisep/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eduwisep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/app_export.dart
@@ -0,0 +1,9 @@
+export 'package:eduwisep/core/utils/image_constant.dart';
+export 'package:eduwisep/core/utils/size_utils.dart';
+export 'package:eduwisep/routes/app_routes.dart';
+export 'package:eduwisep/theme/app_decoration.dart';
+export 'package:eduwisep/theme/custom_text_style.dart';
+export 'package:eduwisep/theme/theme_helper.dart';
+export 'package:eduwisep/widgets/custom_image_view.dart';
+export '../theme/custom_button_style.dart';
+export 'package:eduwisep/core/utils/date_time_utils.dart';
